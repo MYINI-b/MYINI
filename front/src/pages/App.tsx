b6cda48c7f3613055650e144b7bd89cbb1b6ce31
hys6078 <hys6078@naver.com>
2022-10-24 16:37:30 +0900

    feat: 🎸 FE router 연결

--- front/src/pages/App.tsx
+++ front/src/pages/App.tsx
@@ -1,25 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
+
 import './global.scss';
+import Onboarding from './Onboarding';
 
 function App() {
   return (
-    <div className="App">
-      app.tsx
-      <div>
-        <h1>다람쥐 헌 쳇바퀴에 타고파</h1>
-        <h2>다람쥐 헌 쳇바퀴에 타고파</h2>
-        <h3>다람쥐 헌 쳇바퀴에 타고파</h3>
-        asdfsaf{' '}
-        <span>
-          sdafas{' '}
-          <div>
-            a dsfadfsasdf sdfasdfasdfsadf<h1>다람쥐 헌 쳇바퀴에 타고파</h1>
-            <h2>다람쥐 헌 쳇바퀴에 타고파</h2>
-            <h3>다람쥐 헌 쳇바퀴에 타고파</h3>
-          </div>
-        </span>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {' '}
+        <Route path="/" element={<Onboarding />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
