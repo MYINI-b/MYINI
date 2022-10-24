182ee71e697ee1af50a9be3840513fb28e12091b
kkk <rkarud1234@koreatech.ac.kr>
2022-10-24 16:29:21 +0900

    feat: 🎸 Project 엔티티 및 Repository 추가

---
+++ back/myini-domain/src/main/java/com/ssafy/myini/project/domain/ProjectRepository.java
@@ -0,0 +1,6 @@
+package com.ssafy.myini.project.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
