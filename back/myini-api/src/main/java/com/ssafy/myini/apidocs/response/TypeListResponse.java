2af46a0028035829cae06bee41e52e654033e977
gksekqls9808 <gksekqls9808@naver.com>
2022-10-25 17:57:14 +0900

    feat: 🎸 TypeListResponse 생성

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/TypeListResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/TypeListResponse.java
@@ -1,2 +1,15 @@
-package com.ssafy.myini.apidocs.response;public class TypeListResponse {
+package com.ssafy.myini.apidocs.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TypeListResponse {
+    private List<PrimitiveTypeResponse> primitiveTypeResponses;
+    private List<ClassTypeResponse> classTypeResponses;
 }
