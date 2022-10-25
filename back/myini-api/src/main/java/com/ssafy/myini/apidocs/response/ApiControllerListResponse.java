970c164e6ba8d80761fcbea665580a117bd8e22c
gksekqls9808 <gksekqls9808@naver.com>
2022-10-25 17:51:33 +0900

    feat: 🎸 ApiControllerListResponse 생성
    ✅ Closes: #319

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ApiControllerListResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ApiControllerListResponse.java
@@ -1,2 +1,13 @@
-package com.ssafy.myini.apidocs.response;public class ApiControllerListResponse {
+package com.ssafy.myini.apidocs.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiControllerListResponse {
+    private Long apiControllerId;
+    private String apiControllerName;
 }
