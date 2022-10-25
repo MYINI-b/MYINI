7ea62d5b5f4b61d215dd9afa62513ad0c3175a6f
gksekqls9808 <gksekqls9808@naver.com>
2022-10-25 17:55:38 +0900

    feat: 🎸 PathVariableResponse 생성
    ✅ Closes: #319

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/PathVariableResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/PathVariableResponse.java
@@ -1,2 +1,14 @@
-package com.ssafy.myini.apidocs.response;public class PathVariableResponse {
+package com.ssafy.myini.apidocs.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PathVariableResponse {
+    private Long pathVariableId;
+    private String pathVariableKey;
+    private String pathVariableType;
 }
