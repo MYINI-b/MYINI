52c70551acb9a7ae605445a184d1397043609f9f
gksekqls9808 <gksekqls9808@naver.com>
2022-10-25 17:54:37 +0900

    feat: 🎸 DtoResponse 생성
    ✅ Closes: #319

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/DtoResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/DtoResponse.java
@@ -1,2 +1,17 @@
-package com.ssafy.myini.apidocs.response;public class DtoResponse {
+package com.ssafy.myini.apidocs.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DtoResponse {
+    private Long dtoId;
+    private String dtoName;
+    private String dtoType;
+    List<DtoItemResponse> dtoItemResponses;
 }
