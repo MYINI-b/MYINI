5f484edef6199fae860625a4ac13f1a325c50a57
gksekqls9808 <gksekqls9808@naver.com>
2022-10-25 17:56:41 +0900

    feat: 🎸 QueryStringResponse
    g

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/QueryStringResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/QueryStringResponse.java
@@ -1,2 +1,14 @@
-package com.ssafy.myini.apidocs.response;public class QueryStringResponse {
+package com.ssafy.myini.apidocs.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QueryStringResponse {
+    private Long queryStringId;
+    private String queryStringKey;
+    private String queryStringType;
 }
