e79490519cb85f7696662321ab48e222fff1523c
gksekqls9808 <gksekqls9808@naver.com>
2022-10-25 17:53:20 +0900

    feat: 🎸 ClassTypeResponse 생성
    ✅ Closes: #319

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ClassTypeResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ClassTypeResponse.java
@@ -1,2 +1,13 @@
-package com.ssafy.myini.apidocs.response;public class ClassTypeResponse {
+package com.ssafy.myini.apidocs.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClassTypeResponse {
+    private Long dtoId;
+    private String dtoName;
 }
