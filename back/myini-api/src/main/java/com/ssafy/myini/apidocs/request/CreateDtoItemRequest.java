adaafedbf4182f846337e1cdd99e9d46e2b066c0
gksekqls9808 <gksekqls9808@naver.com>
2022-10-25 17:44:09 +0900

    feat: 🎸 CreateDtoItemRequest DTO 생성
    ✅ Closes: #319

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/request/CreateDtoItemRequest.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/request/CreateDtoItemRequest.java
@@ -1,4 +1,15 @@
 package com.ssafy.myini.apidocs.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateDtoItemRequest {
+    private String dtoItemName;
+    private Long dtoClassType;
+    private Long dtoPrimitiveType;
+    private String dtoIsList;
 }
