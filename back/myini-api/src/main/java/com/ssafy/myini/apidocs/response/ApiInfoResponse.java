5504ff20ba7a2dfe40ea6547944d6b562a33d43b
gksekqls9808 <gksekqls9808@naver.com>
2022-10-25 17:52:20 +0900

    feat: 🎸 ApiInfoResponse 생성
    ✅ Closes: #319

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ApiInfoResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ApiInfoResponse.java
@@ -1,2 +1,21 @@
-package com.ssafy.myini.apidocs.response;public class ApiInfoResponse {
+package com.ssafy.myini.apidocs.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiInfoResponse {
+    private ApiResponse apiResponse;
+
+    private List<PathVariableResponse> pathVariableResponses;
+
+    private List<QueryStringResponse> queryStringResponses;
+
+    private List<DtoResponse> dtoResponses;
+
 }
