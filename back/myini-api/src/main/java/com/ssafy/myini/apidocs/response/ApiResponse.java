3debd70c9ae3ae5b42cf3d56401d5cf88d44648a
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:42:12 +0900

    feat: 🎸 ApiResponse 수정

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ApiResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ApiResponse.java
@@ -1,12 +1,13 @@
 package com.ssafy.myini.apidocs.response;
 
+import com.querydsl.core.annotations.QueryProjection;
+import com.ssafy.myini.apidocs.domain.Api;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class ApiResponse {
     private Long apiId;
     private int apiItemId;
@@ -14,4 +15,15 @@ public class ApiResponse {
     private String apiUrl;
     private String apiMethod;
     private String apiCode;
+
+    public static ApiResponse from(Api api){
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.apiId = api.getApiId();
+        apiResponse.apiItemId = api.getApiItemId();
+        apiResponse.apiName = api.getApiName();
+        apiResponse.apiUrl = api.getApiUrl();
+        apiResponse.apiMethod = String.valueOf(api.getApiMethod());
+        apiResponse.apiCode = String.valueOf(api.getApiCode());
+        return apiResponse;
+    }
 }
