d854648db7036f6a220f615865e5d8e95ceef18c
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:41:15 +0900

    feat: 🎸 ApiDocsQueryRepository extends 추가

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/query/ApiDocsQueryRepository.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/query/ApiDocsQueryRepository.java
@@ -1,4 +1,50 @@
 package com.ssafy.myini.apidocs.query;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.myini.apidocs.domain.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+
+import static com.ssafy.myini.apidocs.domain.QApi.api;
+import static com.ssafy.myini.apidocs.domain.QApiController.apiController;
+import static com.ssafy.myini.apidocs.domain.QDto.dto;
+import static com.ssafy.myini.apidocs.domain.QDtoItem.dtoItem;
+import static com.ssafy.myini.apidocs.domain.QPathVariable.pathVariable;
+import static com.ssafy.myini.apidocs.domain.QQueryString.queryString;
+
+@Repository
+@RequiredArgsConstructor
 public class ApiDocsQueryRepository {
+    private final JPAQueryFactory queryFactory;
+
+    public ApiController findByApiControllerId(ApiController findApiController){
+        return queryFactory
+                .selectFrom(apiController)
+                .join(apiController, api.apiController).fetchJoin()
+                .where(apiController.eq(findApiController))
+                .fetchOne();
+    }
+
+    public Api findByApiId(Api findApi){
+        return queryFactory
+                .selectFrom(api)
+                .leftJoin(api, pathVariable.api)
+                .leftJoin(api, queryString.api)
+                .leftJoin(api, dto.api)
+                .where(api.eq(findApi))
+                .fetchOne();
+    }
+
+    public Dto findByDtoId(Dto findDto){
+        return queryFactory
+                .selectFrom(dto)
+                .join(dto, dtoItem.dto).fetchJoin()
+                .where(dto.eq(findDto))
+                .fetchOne();
+    }
+
+    public Dto findByProjectId(){
+        return null;
+    }
 }
