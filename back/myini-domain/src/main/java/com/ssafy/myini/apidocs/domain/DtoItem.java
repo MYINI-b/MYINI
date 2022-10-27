e78bd3dc7226bed09481cc7c26ab632b55d135be
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:47:59 +0900

    feat: 🎸 생성 수정 로직 추가

--- back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/DtoItem.java
+++ back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/DtoItem.java
@@ -34,4 +34,19 @@ public class DtoItem {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "primitive_id")
     private Primitive primitive;
+
+    public static DtoItem createDtoItem(String dtoItemName, Dto dtoClassType, Primitive primitive, String dtoIsList){
+        DtoItem dtoItem = new DtoItem();
+        dtoItem.dtoItemName = dtoItemName;
+        dtoItem.dtoClassType = dtoClassType;
+        dtoItem.primitive = primitive;
+        dtoItem.dtoIsList = YN.valueOf(dtoIsList);
+        return dtoItem;
+    }
+    public void updateDtoItem(String dtoItemName, Dto dtoClassType, Primitive primitive, String dtoIsList){
+        this.dtoItemName = dtoItemName;
+        this.dtoClassType = dtoClassType;
+        this.primitive = primitive;
+        this.dtoIsList = YN.valueOf(dtoIsList);
+    }
 }
