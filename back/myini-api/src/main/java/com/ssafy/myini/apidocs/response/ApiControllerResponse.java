2c2ddac4bf8450d75b42e3ba11000a21d31f275e
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:41:41 +0900

    feat: 🎸 ApiControllerResponse 수정

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ApiControllerResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ApiControllerResponse.java
@@ -1,18 +1,33 @@
 package com.ssafy.myini.apidocs.response;
 
+import com.querydsl.core.annotations.QueryProjection;
+import com.ssafy.myini.apidocs.domain.ApiController;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class ApiControllerResponse {
     private Long apiControllerId;
     private String apiControllerName;
     private String apiControllerBaseUrl;
     private String apiControllerDescription;
     private List<ApiResponse> apiResponses;
+
+    public static ApiControllerResponse from(ApiController apiController){
+        ApiControllerResponse apiControllerResponse = new ApiControllerResponse();
+        apiControllerResponse.apiControllerId = apiController.getApiControllerId();
+        apiControllerResponse.apiControllerName = apiController.getApiControllerName();
+        apiControllerResponse.apiControllerBaseUrl = apiController.getApiControllerBaseUrl();
+        apiControllerResponse.apiControllerDescription = apiController.getApiControllerDescription();
+        apiControllerResponse.apiResponses = apiController.getApis().stream()
+                .map(ApiResponse::from)
+                .collect(Collectors.toList());
+        return apiControllerResponse;
+    }
 }
