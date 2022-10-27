f4cc7181005f02da601ef122b8b12d47460f743a
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:47:10 +0900

    feat: 🎸 상속관계 추가

--- back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/ApiControllerRepository.java
+++ back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/ApiControllerRepository.java
@@ -1,2 +1,10 @@
-package com.ssafy.myini.apidocs.domain;public interface ApiControllerRepository {
+package com.ssafy.myini.apidocs.domain;
+
+import com.ssafy.myini.project.domain.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ApiControllerRepository extends JpaRepository<ApiController, Long> {
+    List<ApiController> findByProject(Project project);
 }
