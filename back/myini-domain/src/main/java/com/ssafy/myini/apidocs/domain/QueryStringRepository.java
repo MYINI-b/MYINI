1500477fc00090178131087899910b7fc271b2ab
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:50:26 +0900

    feat: 🎸 상속관계 추가

--- back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/QueryStringRepository.java
+++ back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/QueryStringRepository.java
@@ -1,2 +1,6 @@
-package com.ssafy.myini.apidocs.domain;public interface QueryStringRepository {
+package com.ssafy.myini.apidocs.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QueryStringRepository extends JpaRepository<QueryString, Long> {
 }
