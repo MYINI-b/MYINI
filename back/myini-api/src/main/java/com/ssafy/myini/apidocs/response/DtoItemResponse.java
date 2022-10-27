eed28ea8cb4fad245b623c5c8e0e843594ce9f14
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:43:21 +0900

    feat: 🎸 DtoItemResponse from 생성

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/DtoItemResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/DtoItemResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.myini.apidocs.response;
 
+import com.ssafy.myini.apidocs.domain.DtoItem;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,14 @@ public class DtoItemResponse {
     private Long dtoClassType;
     private Long dtoPrimitiveType;
     private String dtoIsList;
+
+    public static DtoItemResponse from(DtoItem dtoItem){
+        DtoItemResponse dtoItemResponse = new DtoItemResponse();
+        dtoItemResponse.dtoItemId = dtoItem.getDtoItemId();
+        dtoItemResponse.dtoItemName = dtoItem.getDtoItemName();
+        dtoItemResponse.dtoClassType = dtoItem.getDtoClassType().getDtoId();
+        dtoItemResponse.dtoPrimitiveType = dtoItem.getPrimitive().getPrimitiveId();
+        dtoItemResponse.dtoIsList = String.valueOf(dtoItem.getDtoIsList());
+        return dtoItemResponse;
+    }
 }
