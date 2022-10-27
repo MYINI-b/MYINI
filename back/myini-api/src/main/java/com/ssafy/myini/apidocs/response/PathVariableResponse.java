79bccbea03d6f742ec249d7c5e67ef972585919e
gksekqls9808 <gksekqls9808@naver.com>
2022-10-28 02:27:30 +0900

    feat: 🎸 DTO 내용 작성

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/PathVariableResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/PathVariableResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.myini.apidocs.response;
 
+import com.ssafy.myini.apidocs.domain.PathVariable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,12 @@ public class PathVariableResponse {
     private Long pathVariableId;
     private String pathVariableKey;
     private String pathVariableType;
+
+    public static PathVariableResponse from(PathVariable pathVariable){
+        PathVariableResponse pathVariableResponse = new PathVariableResponse();
+        pathVariableResponse.pathVariableId = pathVariable.getPathVariableId();
+        pathVariableResponse.pathVariableKey = pathVariable.getPathVariableKey();
+        pathVariableResponse.pathVariableType = pathVariable.getPathVariableType();
+        return pathVariableResponse;
+    }
 }
