c51b877b3a54f3a53bf5c0f51ae8e43267deda29
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:42:56 +0900

    feat: 🎸 ClassTypeResponse from 메소드 생성

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ClassTypeResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/ClassTypeResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.myini.apidocs.response;
 
+import com.ssafy.myini.apidocs.domain.Dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +11,11 @@ import lombok.NoArgsConstructor;
 public class ClassTypeResponse {
     private Long dtoId;
     private String dtoName;
+
+    public static ClassTypeResponse from(Dto dto){
+        ClassTypeResponse classTypeResponse = new ClassTypeResponse();
+        classTypeResponse.dtoId = dto.getDtoId();
+        classTypeResponse.dtoName = dto.getDtoName();
+        return classTypeResponse;
+    }
 }
