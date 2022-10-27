ade449b8d6dce097d44d58f91b7a73efec170f3f
gksekqls9808 <gksekqls9808@naver.com>
2022-10-28 02:27:53 +0900

    feat: 🎸 DTO 내용작성

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/QueryStringResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/QueryStringResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.myini.apidocs.response;
 
+import com.ssafy.myini.apidocs.domain.QueryString;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,11 @@ public class QueryStringResponse {
     private Long queryStringId;
     private String queryStringKey;
     private String queryStringType;
+    public static QueryStringResponse from(QueryString queryString){
+        QueryStringResponse queryStringResponse = new QueryStringResponse();
+        queryStringResponse.queryStringId = queryString.getQueryStringId();
+        queryStringResponse.queryStringKey = queryString.getQueryStringKey();
+        queryStringResponse.queryStringType = queryString.getQueryStringType();
+        return queryStringResponse;
+    }
 }
