739a7af4216b6a65fbd76b5b15a1e615015fcb26
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:44:47 +0900

    feat: 🎸 ApiDocsService 수정

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/service/ApiDocsService.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/service/ApiDocsService.java
@@ -1,9 +1,7 @@
 package com.ssafy.myini.apidocs.service;
 
 import com.ssafy.myini.apidocs.request.*;
-import com.ssafy.myini.apidocs.response.ApiControllerListResponse;
-import com.ssafy.myini.apidocs.response.ApiControllerResponse;
-import com.ssafy.myini.apidocs.response.ApiInfoResponse;
+import com.ssafy.myini.apidocs.response.*;
 
 import java.util.List;
 
@@ -29,16 +27,29 @@ public interface ApiDocsService {
     //PathVariable 생성
     void createPathVariable(Long projectId, Long apiControllerId, Long apiId, CreatePathVariableRequest request);
     //PathVariable 수정
+    void updatePathVariable(Long projectId, Long apiControllerId, Long apiId, Long pathVariableId, UpdatePathVariableRequest request);
     //PathVariable 삭제
+    void deletePathVariable(Long projectId, Long apiControllerId, Long apiId, Long pathVariableId);
     //QueryString 생성
+    void createQueryString(Long projectId, Long apiControllerId, Long apiId, CreateQueryStringRequest request);
     //QueryString 수정
+    void updateQueryString(Long projectId, Long apiControllerId, Long apiId, Long queryStringId, UpdateQueryStringRequest request);
     //QueryString 삭제
+    void deleteQueryString(Long projectId, Long apiControllerId, Long apiId, Long queryStringId);
     //DTO 생성
+    void createDto(Long projectId, Long apiControllerId, Long apiId, CreateDtoRequest request);
     //DTO 수정
+    void updateDto(Long projectId, Long apiControllerId, Long apiId, Long dtoId, UpdateDtoRequest request);
     //DTO 삭제
+    void deleteDto(Long projectId, Long apiControllerId, Long apiId, Long dtoId);
     //DTO 조회
+    DtoResponse findByDtoId(Long projectId, Long apiControllerId, Long apiId, Long dtoId);
     //DTO 변수 생성
+    void createDtoItem(Long projectId, Long apiControllerId, Long apiId, Long dtoId, CreateDtoItemRequest request);
     //DTO 변수 수정
+    void updateDtoItem(Long projectId, Long apiControllerId, Long apiId, Long dtoId, Long dtoItemId, UpdateDtoItemRequest request);
     //DTO 변수 삭제
+    void deleteDtoItem(Long projectId, Long apiControllerId, Long apiId, Long dtoId, Long dtoItemId);
     //자료형 리스트 조회
+    TypeListResponse findTypeList(Long projectId);
 }
