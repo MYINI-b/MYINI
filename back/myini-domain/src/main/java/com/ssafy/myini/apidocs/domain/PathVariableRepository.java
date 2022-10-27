d4e87649a3eda56ad3ce1320b9cfe2c634ac0014
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:49:45 +0900

    feat: 🎸 상속관계 추가

--- back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/PathVariableRepository.java
+++ back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/PathVariableRepository.java
@@ -1,2 +1,6 @@
-package com.ssafy.myini.apidocs.domain;public interface PathVariableRepository {
+package com.ssafy.myini.apidocs.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PathVariableRepository extends JpaRepository<PathVariable, Long> {
 }
