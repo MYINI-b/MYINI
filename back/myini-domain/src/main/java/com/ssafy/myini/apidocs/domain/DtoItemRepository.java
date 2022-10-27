7fb3c5c8dcc3f435a9ede1eb30bc4ea9c73d128a
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:49:06 +0900

    feat: 🎸 상속관계 추가

--- back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/DtoItemRepository.java
+++ back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/DtoItemRepository.java
@@ -1,2 +1,6 @@
-package com.ssafy.myini.apidocs.domain;public interface DtoItemRepository {
+package com.ssafy.myini.apidocs.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DtoItemRepository extends JpaRepository<DtoItem, Long> {
 }
