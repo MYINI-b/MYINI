4f022f5c3c97c5427f1aa4e182538482a67156d8
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:44:24 +0900

    feat: 🎸 from 메소드 생성

--- back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/TypeListResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/apidocs/response/TypeListResponse.java
@@ -1,10 +1,13 @@
 package com.ssafy.myini.apidocs.response;
 
+import com.ssafy.myini.apidocs.domain.Dto;
+import com.ssafy.myini.apidocs.domain.Primitive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
@@ -12,4 +15,15 @@ import java.util.List;
 public class TypeListResponse {
     private List<PrimitiveTypeResponse> primitiveTypeResponses;
     private List<ClassTypeResponse> classTypeResponses;
+
+    public static TypeListResponse from(List<Primitive> primitives, List<Dto> dtos){
+        TypeListResponse typeListResponse = new TypeListResponse();
+        typeListResponse.primitiveTypeResponses = primitives.stream()
+                .map(PrimitiveTypeResponse::from)
+                .collect(Collectors.toList());
+        typeListResponse.classTypeResponses = dtos.stream()
+                .map(ClassTypeResponse::from)
+                .collect(Collectors.toList());
+        return typeListResponse;
+    }
 }
