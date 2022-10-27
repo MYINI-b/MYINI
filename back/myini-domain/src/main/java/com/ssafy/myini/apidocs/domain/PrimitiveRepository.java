d9c4a3d125da7a5a3ed6fd128e08d872c707c25c
gksekqls9808 <gksekqls9808@naver.com>
2022-10-27 17:50:01 +0900

    feat: 🎸 상속관계 추가

--- back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/PrimitiveRepository.java
+++ back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/PrimitiveRepository.java
@@ -1,2 +1,6 @@
-package com.ssafy.myini.apidocs.domain;public interface PrimitiveRepository {
+package com.ssafy.myini.apidocs.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrimitiveRepository extends JpaRepository<Primitive, Long> {
 }
