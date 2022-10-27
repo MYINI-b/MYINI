b61e488c8ab1750e86c24e110fd01a3e1b895c50
hys6078 <hys6078@naver.com>
2022-10-27 17:05:49 +0900

    fix: 🐛 API 명세서 쿼리 작성에 따른 path variables 작성 로직 버그 수정
    ✅ Closes: #260

--- front/src/pages/ApiSpec/ApiAddModal/ApiContentLeft/index.tsx
+++ front/src/pages/ApiSpec/ApiAddModal/ApiContentLeft/index.tsx
@@ -14,7 +14,7 @@ interface QUERY {
 
 export default function ApiContentLeft() {
   const [isQuery, setIsQuery] = useState(false);
-  const [apiUrl, setApiUrl] = useState('exercise');
+  const [apiUrl, setApiUrl] = useState('');
   const [pathVarList, setPathVarList] = useState<Array<PATHVARIABLES>>([
     { key: '' },
   ]);
@@ -24,8 +24,38 @@ export default function ApiContentLeft() {
 
   const onKeyChange = useCallback(
     (idx: number, e: any) => {
+      const parsedApiUrl = apiUrl.split('?');
       if (isQuery) {
-        console.log('Sdf');
+        const copyList = [...queryList];
+        const copyObj = { ...queryList[idx] };
+        copyObj.key = e.target.value;
+        copyList[idx] = copyObj;
+
+        if (e.target.value !== '' && idx === queryList.length - 1) {
+          copyList.push({ key: '', type: '' });
+        }
+
+        const divisionIdx = apiUrl.indexOf('?');
+
+        if (divisionIdx < 0) {
+          setApiUrl(`${apiUrl}?${e.target.value}=`);
+        } else {
+          const onlyQueryUrl = apiUrl.substring(
+            divisionIdx + 1,
+            apiUrl.length - 1,
+          );
+          if (onlyQueryUrl === '') setApiUrl(apiUrl.substring(0, divisionIdx));
+          else {
+            let parsedQueryArray = onlyQueryUrl.split('&');
+            parsedQueryArray[idx] = e.target.value;
+            parsedQueryArray = parsedQueryArray.map(
+              (query) => `${query.replaceAll('=', '')}=`,
+            );
+            setApiUrl(`${apiUrl.split('?')[0]}?${parsedQueryArray.join('&')}`);
+          }
+        }
+
+        setQueryList([...copyList]);
       } else {
         const copyList = [...pathVarList];
         const copyObj = { ...copyList[idx] };
@@ -34,38 +64,84 @@ export default function ApiContentLeft() {
         let parsedApiUrlArray = apiUrl
           .split('/')
           .map((url) => url.replaceAll('{', '').replaceAll('}', ''));
+
         // 마지막 인덱스 수정시작하면 새거 추가
         if (e.target.value !== '' && idx === pathVarList.length - 1) {
           copyList.push({ key: '' });
         }
-        if (parsedApiUrlArray.length === 0)
-          setApiUrl((prev) => `${prev}/{${e.target.value}}`);
-        else {
-          parsedApiUrlArray[idx + 1] = e.target.value;
-          parsedApiUrlArray = parsedApiUrlArray.map((url, i) =>
-            i === 0 ? url : url === '' ? '' : `{${url}}`,
+
+        console.log(parsedApiUrlArray);
+        // 첫 path variable 추가라면
+        if (parsedApiUrlArray.length === 1)
+          setApiUrl(
+            `/{${e.target.value}}${
+              parsedApiUrl.length > 1 ? `?${parsedApiUrl[1]}` : ''
+            }`,
           );
-          setApiUrl(parsedApiUrlArray.join('/'));
+        else {
+          if (e.target.value === '') {
+            copyList.splice(idx, 1);
+          }
+          parsedApiUrlArray[idx + 1] = e.target.value;
+          parsedApiUrlArray = parsedApiUrlArray
+            .filter((url, i) => i === 0 || url !== '')
+            .map((url, i) => (i === 0 ? url : `{${url}}`));
+
+          setApiUrl(
+            `${parsedApiUrlArray.join('/')}${
+              parsedApiUrl.length > 1 ? `?${parsedApiUrl[1]}` : ''
+            }`,
+          );
         }
         setPathVarList([...copyList]);
       }
     },
-    [isQuery, pathVarList, apiUrl],
+    [isQuery, pathVarList, apiUrl, queryList],
   );
 
   const deleteKey = useCallback(
     (idx: number) => {
-      const copyList = [...pathVarList];
-      if (idx === 0 && pathVarList.length === 1) return;
+      if (!isQuery) {
+        const copyList = [...pathVarList];
+        const onlyApiUrl = apiUrl.split('?')[0];
+        const divisionIdx = apiUrl.indexOf('?');
 
-      const parsedApiUrlArray = apiUrl.split('/');
-      copyList.splice(idx, 1);
-      parsedApiUrlArray.splice(idx + 1, 1);
+        if (idx === 0 && pathVarList.length === 1) {
+          setApiUrl(apiUrl.substring(0, divisionIdx));
+          setQueryList([{ key: '', type: '' }]);
+          return;
+        }
 
-      setApiUrl(parsedApiUrlArray.join('/'));
-      setPathVarList([...copyList]);
+        const parsedApiUrlArray = apiUrl.split('/');
+        copyList.splice(idx, 1);
+        parsedApiUrlArray.splice(idx + 1, 1);
+
+        setApiUrl(parsedApiUrlArray.join('/'));
+        setPathVarList([...copyList]);
+      } else {
+        const copyList = [...queryList];
+        const onlyQueryUrl = apiUrl.split('?')[1];
+        const divisionIdx = apiUrl.indexOf('?');
+
+        if (idx === 0 && queryList.length === 1) {
+          setApiUrl(apiUrl.substring(0, divisionIdx));
+          setQueryList([{ key: '', type: '' }]);
+          return;
+        }
+
+        const parsedQueryArray = onlyQueryUrl.split('&');
+        copyList.splice(idx, 1);
+        parsedQueryArray.splice(idx, 1);
+
+        setApiUrl(
+          `${apiUrl.substring(0, divisionIdx)}${
+            parsedQueryArray.length > 0 ? `?${parsedQueryArray.join('&')}` : ''
+          }`,
+        );
+        setQueryList([...copyList]);
+      }
     },
-    [pathVarList, apiUrl],
+    [pathVarList, apiUrl, isQuery, queryList],
   );
 
   return (
@@ -94,7 +170,7 @@ export default function ApiContentLeft() {
         <input
           type="text"
           className="api-url-input"
-          value={`/${apiUrl}`}
+          value={`/exercise${apiUrl}`}
           readOnly
         />
       </div>
@@ -115,40 +191,48 @@ export default function ApiContentLeft() {
           </h3>
         </div>
         <div className="api-query-content-container">
-          {!isQuery ? (
-            pathVarList.map((pathvar, i) => {
-              return (
-                <div className="api-query-input-container" key={i}>
-                  <input
-                    type="text"
-                    className="api-query-input"
-                    placeholder="KEY"
-                    onChange={(e) => onKeyChange(i, e)}
-                    value={pathvar.key}
-                  />
-                  <FontAwesomeIcon
-                    icon={faClose}
-                    className="api-query-delete"
-                    onClick={() => deleteKey(i)}
-                  />
-                </div>
-              );
-            })
-          ) : (
-            <div className="api-query-input-container">
-              <input
-                type="text"
-                className="api-query-input"
-                placeholder="KEY"
-              />
-              <input
-                type="text"
-                className="api-query-input"
-                placeholder="TYPE"
-              />
-              <FontAwesomeIcon icon={faClose} className="api-query-delete" />
-            </div>
-          )}
+          {!isQuery
+            ? pathVarList.map((pathvar, i) => {
+                return (
+                  <div className="api-query-input-container" key={i}>
+                    <input
+                      type="text"
+                      className="api-query-input"
+                      placeholder="KEY"
+                      onChange={(e) => onKeyChange(i, e)}
+                      value={pathvar.key}
+                    />
+                    <FontAwesomeIcon
+                      icon={faClose}
+                      className="api-query-delete"
+                      onClick={() => deleteKey(i)}
+                    />
+                  </div>
+                );
+              })
+            : queryList.map((query, i) => {
+                return (
+                  <div className="api-query-input-container" key={i}>
+                    <input
+                      type="text"
+                      className="api-query-input"
+                      placeholder="KEY"
+                      onChange={(e) => onKeyChange(i, e)}
+                      value={query.key}
+                    />
+                    <input
+                      type="text"
+                      className="api-query-input"
+                      placeholder="TYPE"
+                    />
+                    <FontAwesomeIcon
+                      icon={faClose}
+                      className="api-query-delete"
+                      onClick={() => deleteKey(i)}
+                    />
+                  </div>
+                );
+              })}
         </div>
       </div>
     </div>
