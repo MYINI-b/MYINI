3ab73ae08857fba0a8b54fe13bfb94db29055b55
hys6078 <hys6078@naver.com>
2022-10-28 09:03:34 +0900

    feat: 🎸 API명세서 api추가 모달 폼 UI 작성
    ✅ Closes: #260

--- front/src/pages/ApiSpec/ApiAddModal/ApiContentRight/index.tsx
+++ front/src/pages/ApiSpec/ApiAddModal/ApiContentRight/index.tsx
@@ -21,12 +21,31 @@ export default function ApiContentRight() {
                 placeholder="변수명을 입력해주세요"
               />
             </div>
+            <div className="content-right-detail-boxcontent">asd</div>
           </div>
         </div>
       </section>
+
       <section className="content-section">
         <h1 className="content-right-title">RESPONSE BODY</h1>
-        <div className="content-right-box">d</div>
+        <div className="content-right-box">
+          <div className="content-right-boxtitle-wrapper">
+            <h3 className="content-right-boxtitle static">자료형</h3>
+            <h3 className="content-right-boxtitle">변수명</h3>
+          </div>
+          <div className="content-right-boxcontent-wrapper">
+            <div className="content-right-normal-boxcontent">
+              <div className="datatype-wrapper">
+                <input type="text" className="datatype-block" value="Striang" />
+              </div>
+              <input
+                type="text"
+                className="content-right-boxcontent-input"
+                placeholder="변수명을 입력해주세요"
+              />
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
