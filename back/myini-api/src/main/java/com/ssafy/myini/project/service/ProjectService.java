4d0705f5232c10bdd194a21fdc3061390fd44fcf
gksekqls9808 <gksekqls9808@naver.com>
2022-10-28 17:14:10 +0900

    feat: 🎸 ProjectService 잓성

--- back/myini-api/src/main/java/com/ssafy/myini/project/service/ProjectService.java
+++ back/myini-api/src/main/java/com/ssafy/myini/project/service/ProjectService.java
@@ -1,2 +1,39 @@
-package com.ssafy.myini.project.service;public interface ProjectService {
+package com.ssafy.myini.project.service;
+
+import com.ssafy.myini.member.domain.Member;
+import com.ssafy.myini.project.request.FindByMemberEmailRequest;
+import com.ssafy.myini.project.request.UpdateProjectRequest;
+import com.ssafy.myini.project.response.ProjectInfoResponse;
+import com.ssafy.myini.project.response.ProjectListResponse;
+import com.ssafy.myini.project.response.ProjectMemberResponse;
+
+import java.util.List;
+
+public interface ProjectService {
+    // 프로젝트 등록
+    void createProject(Member member);
+
+    // 프로젝트 전체 리스트
+    List<ProjectListResponse> findAll(Member member);
+
+    // 프로젝트 단건 조회
+    ProjectInfoResponse findByProjectId(Long projectId);
+
+    // 프로젝트 수정
+    void updateProject(Long projectId, UpdateProjectRequest request);
+
+    // 프로젝트 삭제
+    void deleteProject(Long projectId);
+
+    // 프로젝트 팀원 리스트 조회
+    List<ProjectMemberResponse> findProjectMemberList(Long projectId);
+
+    // 프로젝트 팀원 검색
+    ProjectMemberResponse findByMemberEmail(FindByMemberEmailRequest request);
+
+    // 프로젝트 팀원 추가
+    void addProjectMember(Long projectId, Long memberId);
+
+    // 프로젝트 팀원 삭제
+    void deleteProjectMember(Long projectId, Long memberId);
 }
