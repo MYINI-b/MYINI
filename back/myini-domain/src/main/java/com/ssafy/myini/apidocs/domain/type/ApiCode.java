334b39788c26713062216318bd3291e13ff9a56a
gksekqls9808 <gksekqls9808@naver.com>
2022-10-28 13:09:43 +0900

    feat: 🎸 ENUM클래스 수정

--- back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/type/ApiCode.java
+++ back/myini-domain/src/main/java/com/ssafy/myini/apidocs/domain/type/ApiCode.java
@@ -1,9 +1,6 @@
 package com.ssafy.myini.apidocs.domain.type;
 
 public enum ApiCode {
-    OK(200), CREATED(201);
-    private int value;
-    private ApiCode(int value){
-        this.value = value;
-    }
+    OK,
+    CREATED
 }
