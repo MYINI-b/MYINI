7433e413bc6d8dbd55c7794dcb01ca5f54508f89
gksekqls9808 <gksekqls9808@naver.com>
2022-10-28 17:13:08 +0900

    feat: 🎸 DTO 생성

--- back/myini-api/src/main/java/com/ssafy/myini/project/response/ProjectInfoResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/project/response/ProjectInfoResponse.java
@@ -1,2 +1,22 @@
-package com.ssafy.myini.project.response;public class ProjectInfoResponse {
+package com.ssafy.myini.project.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectInfoResponse {
+    private String projectName;
+    private String projectDescription;
+    private String projectImg;
+    private LocalDate projectStartedDate;
+    private LocalDate projectfinishedDate;
+    private String projectGithubUrl;
+    private String proejctJiraUrl;
+    private String proejctNotionUrl;
+    private String proejctFigmaUrl;
 }
