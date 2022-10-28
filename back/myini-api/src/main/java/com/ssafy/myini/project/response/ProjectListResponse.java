3d0b3b1a3e8d053c5b0a25cd6553e391ededda70
gksekqls9808 <gksekqls9808@naver.com>
2022-10-28 17:13:29 +0900

    feat: 🎸 DTO 생성
    \

--- back/myini-api/src/main/java/com/ssafy/myini/project/response/ProjectListResponse.java
+++ back/myini-api/src/main/java/com/ssafy/myini/project/response/ProjectListResponse.java
@@ -1,2 +1,18 @@
-package com.ssafy.myini.project.response;public class ProjectListResponse {
+package com.ssafy.myini.project.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectListResponse {
+    private String projectName;
+    private String projectDescription;
+    private String projectImg;
+    private List<ProjectMemberResponse> projectMemberResponses;
+
 }
