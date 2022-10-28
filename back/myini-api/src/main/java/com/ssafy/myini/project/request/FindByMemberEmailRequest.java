3130fe5279499e614a849f902b6e2ef21202e327
gksekqls9808 <gksekqls9808@naver.com>
2022-10-28 17:12:21 +0900

    feat: 🎸 DTO 생성

--- back/myini-api/src/main/java/com/ssafy/myini/project/request/FindByMemberEmailRequest.java
+++ back/myini-api/src/main/java/com/ssafy/myini/project/request/FindByMemberEmailRequest.java
@@ -1,2 +1,12 @@
-package com.ssafy.myini.project.request;public class FindByMemberEmail {
+package com.ssafy.myini.project.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FindByMemberEmailRequest {
+    private String memberEmail;
 }
