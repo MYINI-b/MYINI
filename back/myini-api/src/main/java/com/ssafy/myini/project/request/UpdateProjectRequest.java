40675e63c504929ef209ddc84b12c06618c0e984
gksekqls9808 <gksekqls9808@naver.com>
2022-10-28 17:12:44 +0900

    feat: 🎸 DTO 생성

--- back/myini-api/src/main/java/com/ssafy/myini/project/request/UpdateProjectRequest.java
+++ back/myini-api/src/main/java/com/ssafy/myini/project/request/UpdateProjectRequest.java
@@ -1,2 +1,22 @@
-package com.ssafy.myini.project.request;public class UpdateProjectRequest {
+package com.ssafy.myini.project.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateProjectRequest {
+    private String projectName;
+    private String projectDescription;
+    private String projectImg;
+    private LocalDate projectStartedDate;
+    private LocalDate projectFinishedDate;
+    private String projectGithubUrl;
+    private String projectJiraUrl;
+    private String projectNotionUrl;
+    private String projectFigmaUrl;
 }
