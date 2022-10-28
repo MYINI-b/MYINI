4c4011df0f81d84616cd1a9c4831c518b4fce2da
이성재 <lsuksa1@naver.com>
2022-10-28 09:33:34 +0900

    ✨ feat: 프로젝트빌 confirmcode 아코디언 추

--- front/src/pages/Build/Accordion/index.tsx
+++ front/src/pages/Build/Accordion/index.tsx
@@ -1,65 +1,74 @@
 /* eslint-disable react/function-component-definition */
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+import './style.scss';
 
 const data = {
   accordion: [
     {
-      question: 'Who I am ?',
-      answer:
-        'I am a Muslim, with an Arabic tongue, of Egyptian birth and of Nubian origins',
+      id: 0,
+      title: 'Controller',
+      body: '무슨내용?',
     },
     {
-      question: 'What I do ?',
-      answer: 'I am currently learning to code and study Islam',
+      id: 1,
+      title: 'Repository',
+      body: '내용2',
     },
     {
-      question: 'What I want to do ?',
-      answer: 'I want to work as Freelance Web Developer or Frontend Developer',
+      id: 2,
+      title: 'Entity',
+      body: '내용3',
     },
     {
-      question: 'Why you should hire me ?',
-      answer: 'Because',
-    },
-    {
-      question: 'question5',
-      answer: 'answer5',
+      id: 3,
+      title: 'etc..',
+      body: '내용4',
     },
   ],
 };
 
 interface DataProps {
   accordion: {
-    question: string;
-    answer: string;
+    id: number;
+    title: string;
+    body: string;
   }[];
 }
 
 const Accordion: React.FC = () => {
   const { accordion } = data as DataProps;
-  const [questionNum, setQuestionNum] = useState<number>(-1);
+  const [titleNum, setTitleNum] = useState<number>(-1);
 
-  const showAnswer = (index: number): void => {
-    if (questionNum === index) {
-      setQuestionNum(-1);
+  const showBody = (index: number): void => {
+    if (titleNum === index) {
+      setTitleNum(-1);
       return;
     }
-    setQuestionNum(index);
+    setTitleNum(index);
   };
 
   return (
     <div>
       {accordion.map((item, index) => (
         <div
-          key={index}
-          className="question__card"
-          onClick={() => showAnswer(index)}
+          role="presentation"
+          key={item.id}
+          className="title-card"
+          onClick={() => showBody(index)}
         >
-          <div className="question__heading">
-            <h3>{item.question}</h3>
-            {/* {questionNum === index ? <IoIosArrowUp /> : <IoIosArrowDown />} */}
+          <div className="title-heading">
+            <h3 className="title-item">{item.title}</h3>
+            {titleNum === index ? (
+              <FontAwesomeIcon icon={faChevronUp} />
+            ) : (
+              <FontAwesomeIcon icon={faChevronDown} />
+            )}
           </div>
-          <p className={`question__body ${questionNum === index && 'active'}`}>
-            {item.answer}
+          <p className={`title-body ${titleNum === index && 'active'}`}>
+            {item.body}
           </p>
         </div>
       ))}
