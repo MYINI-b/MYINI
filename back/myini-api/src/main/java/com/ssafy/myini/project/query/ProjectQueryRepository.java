6efc47b321652a8ff28def40afd98f255fd908cd
gksekqls9808 <gksekqls9808@naver.com>
2022-10-31 01:39:05 +0900

    feat: 🎸 project api 쿼리 작성

--- back/myini-api/src/main/java/com/ssafy/myini/project/query/ProjectQueryRepository.java
+++ back/myini-api/src/main/java/com/ssafy/myini/project/query/ProjectQueryRepository.java
@@ -1,4 +1,45 @@
 package com.ssafy.myini.project.query;
 
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.myini.member.domain.Member;
+import com.ssafy.myini.member.domain.MemberProject;
+import com.ssafy.myini.member.domain.QMember;
+import com.ssafy.myini.member.domain.QMemberProject;
+import com.ssafy.myini.project.domain.Project;
+import com.ssafy.myini.project.domain.QProject;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.ssafy.myini.member.domain.QMember.member;
+import static com.ssafy.myini.member.domain.QMemberProject.memberProject;
+import static com.ssafy.myini.project.domain.QProject.project;
+
+@Repository
+@RequiredArgsConstructor
 public class ProjectQueryRepository {
+    private final JPAQueryFactory queryFactory;
+
+    public List<MemberProject> findProjectMemberList(Long projectId){
+        return queryFactory
+                .selectFrom(memberProject)
+                .join(memberProject.member, member).fetchJoin()
+                .where(memberProject.project.projectId.eq(projectId))
+                .fetch();
+    }
+
+    public List<Project> findAll(Member findMember){
+        QMemberProject conditionMemberProject = new QMemberProject("condition_member_project");
+
+        return queryFactory
+                .select(project).distinct()
+                .from(conditionMemberProject)
+                .join(conditionMemberProject.project, project).fetchJoin()
+                .join(project.memberProjects, memberProject).fetchJoin()
+                .join(memberProject.member, member).fetchJoin()
+                .where(conditionMemberProject.member.eq(findMember))
+                .fetch();
+    }
 }
