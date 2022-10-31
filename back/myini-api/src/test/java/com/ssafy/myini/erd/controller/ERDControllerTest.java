f0a6362e94773671948fb3a2271c22e7ca829b5d
rladnqsl98 <rladnqls98@gmail.com>
2022-10-31 14:44:28 +0900

    feat: 🎸 db주소 local to server변경

---
+++ back/myini-api/src/test/java/com/ssafy/myini/erd/controller/ERDControllerTest.java
@@ -0,0 +1,52 @@
+package com.ssafy.myini.erd.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ERDControllerTest {
+
+    @Test
+    void createErdTable() {
+    }
+
+    @Test
+    void findAllErdTable() {
+    }
+
+    @Test
+    void updateErdTable() {
+    }
+
+    @Test
+    void deleteErdTable() {
+    }
+
+    @Test
+    void createTableRelation() {
+    }
+
+    @Test
+    void deleteTableRelation() {
+    }
+
+    @Test
+    void findAllRelationItem() {
+    }
+
+    @Test
+    void findAllConditionItem() {
+    }
+
+    @Test
+    void createTableColumn() {
+    }
+
+    @Test
+    void updateTableColumn() {
+    }
+
+    @Test
+    void deleteTableColumn() {
+    }
+}
