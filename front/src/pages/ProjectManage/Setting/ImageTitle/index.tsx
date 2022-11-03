0e67ec177e06b7c706dde73e110c011f84099442
hys6078 <hys6078@naver.com>
2022-11-03 09:20:31 +0900

    feat: 🎸 이미지 수정 api 연결

--- front/src/pages/ProjectManage/Setting/ImageTitle/index.tsx
+++ front/src/pages/ProjectManage/Setting/ImageTitle/index.tsx
@@ -2,6 +2,7 @@ import { faPen, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faImage } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState, useCallback, Dispatch, useRef } from 'react';
+import axios from 'axios';
 import './style.scss';
 import DefaultProfile from 'assets/default-profile.png';
 import { profile } from 'console';
@@ -40,10 +41,21 @@ export default function ProjectImage({
     reader.onload = async () => {
       if (reader.readyState === 2) {
         // 이미지 정상적으로 불러오면 변경하기
+        formData.append('projectImg', profileImg);
+
+        const dummyAccessToken =
+          'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzIiwiUk9MRSI6IlJPTEVfVVNFUiIsImlhdCI6MTY2NzI2NTcyMywiZXhwIjoxNjY3ODcwNTIzfQ.WSa3oFZmJtaXSdsMM0V46FgRFY53zP5E1sydiorQwgI';
+        const headers = {
+          headers: {
+            Authorization: `Bearer ${dummyAccessToken}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        };
+
+        const resp = await axios.patch('/projects/1/images', formData, headers);
+        console.log(resp);
         if (reader.result && typeof reader.result === 'string')
           setImg(reader.result);
-        formData.append('file', profileImg);
-
         // 유저 이미지 변경 api 전송
       }
     };
