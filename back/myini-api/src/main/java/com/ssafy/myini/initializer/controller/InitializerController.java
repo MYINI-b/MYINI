ff49100940ade720627be1c04a24f2c1e799833f
gksekqls9808 <gksekqls9808@naver.com>
2022-11-04 13:28:15 +0900

    feat: 🎸 이니셜라이저로직 수정

--- back/myini-api/src/main/java/com/ssafy/myini/initializer/controller/InitializerController.java
+++ back/myini-api/src/main/java/com/ssafy/myini/initializer/controller/InitializerController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriUtils;
 
+import javax.validation.Valid;
 import java.io.ByteArrayOutputStream;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
@@ -32,9 +33,9 @@ public class InitializerController {
         return ResponseEntity.ok().body(body);
     }
 
-    @PostMapping("/{projectid}")
+    @GetMapping("/{projectid}")
     public ResponseEntity<Resource> initializerStart(@PathVariable("projectid") Long projectId,
-                                                 @RequestBody InitializerRequest initializerRequest) throws MalformedURLException {
+                                                 @Valid InitializerRequest initializerRequest) throws MalformedURLException {
 
         UrlResource urlResource = initializerService.initializerStart(projectId, initializerRequest);
         String encodedUploadFileName = UriUtils.encode("test", StandardCharsets.UTF_8);
