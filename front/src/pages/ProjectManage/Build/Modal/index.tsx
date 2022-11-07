e33421c43e77b6c71c8e1835f0a5d3076baa2b06
이성재 <lsuksa1@naver.com>
2022-11-07 10:24:45 +0900

    ✨ feat: 프로젝트빌드페이지 api 연결중

---
+++ front/src/pages/ProjectManage/Build/Modal/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.scss';
+
+function Modal({ modalClose }: { modalClose: any }) {
+  const onCloseModal = (e: any) => {
+    // console.log('e.target: ', e.target);
+    // console.log('e.tarcurrentTargetget: ', e.currentTarget);
+    if (e.target === e.currentTarget) {
+      modalClose();
+    }
+  };
+  return (
+    <div className="modal__container" onClick={onCloseModal}>
+      <div className="modal">
+        <div>dd</div>
+        <button type="button" className="modal__button" onClick={modalClose}>
+          {' '}
+          Modal Close
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
--- front/src/pages/ProjectManage/Build/index.tsx
+++ front/src/pages/ProjectManage/Build/index.tsx
@@ -1,20 +1,30 @@
 import './style.scss';
 
 import { useState } from 'react';
+import { getApi } from 'api';
 import Accordion from './Accordion';
 import Buttons from './SelectButton';
-import Tabs from './Tabs';
+import Modal from './Modal';
 
 export default function Build() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const modalClose = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  const [asd, setAsd] = useState<any>([]);
   const [text, setText] = useState('');
   const [isChecked, setChecked] = useState(false);
   const handleTextArea = (e: any) => {
     setText(e.target.value);
+    const asd = getApi('api/initializers/1/preview');
+    console.log(asd);
   };
   const handleSubmit = (e: any) => {
     e.preventDefault();
     alert(`${text}\nchecked? ${isChecked}`);
   };
+  // https://k7b203.p.ssafy.io/api/initializers/{projectid}/preview
   return (
     <div className="build-container">
       <h1 className="build-title">API 명세서</h1>
@@ -22,55 +32,113 @@ export default function Build() {
       <div className="build-main">
         <div className="init-container">
           <div className="title-item">INIT SETTING</div>
-          <Tabs>
-            <span className="tab-item" title="Spring Boot">
-              <div className="item-row">
-                <div className="button-item">
-                  <div className="item-element">Project</div>
-                  <Buttons>
-                    <span title="Maven" />
-                    <span title="Gradle" />
-                  </Buttons>
-                </div>
-                <div className="button-item">
-                  <div className="item-element">Java</div>
-                  <Buttons>
-                    <span title="19" />
-                    <span title="17" />
-                    <span title="11" />
-                    <span title="8" />
-                  </Buttons>
-                </div>
-                <div className="button-item">
-                  <div className="item-element">Spring Boot</div>
-                  <Buttons>
-                    <span title="3.0.0" />
-                    <span title="2.7.5" />
-                    <span title="2.7.4" />
-                    <span title="2.6.13" />
-                  </Buttons>
-                </div>
-                <div className="button-item">
-                  <div className="item-element">Packaging</div>
-                  <Buttons>
-                    <span title="Jar" />
-                    <span title="War" />
-                  </Buttons>
-                </div>
+          <span className="tab-item" title="Spring Boot">
+            <div className="item-row">
+              <div className="button-item">
+                <div className="item-element">Project</div>
+                <Buttons>
+                  <span title="Maven" />
+                  <span title="Gradle" />
+                </Buttons>
               </div>
-              <div>Metadata</div>
+              <div className="button-item">
+                <div className="item-element">Java</div>
+                <Buttons>
+                  <span title="19" />
+                  <span title="17" />
+                  <span title="11" />
+                  <span title="8" />
+                </Buttons>
+              </div>
+              <div className="button-item">
+                <div className="item-element">Language</div>
+                <Buttons>
+                  <span title="Java" />
+                  <span title="Kotlin" />
+                  <span title="Groovy" />
+                </Buttons>
+              </div>
+              <div className="button-item">
+                <div className="item-element">Spring Boot</div>
+                <Buttons>
+                  <span title="3.0.0" />
+                  <span title="2.7.6" />
+                  <span title="2.7.5" />
+                  <span title="2.6.14" />
+                  <span title="2.6.13" />
+                </Buttons>
+              </div>
+              <div className="button-item">
+                <div className="item-element">Packaging</div>
+                <Buttons>
+                  <span title="Jar" />
+                  <span title="War" />
+                </Buttons>
+              </div>
+            </div>
+            <div className="metadata">Metadata</div>
+            <div className="project-metadata">
+              <div className="metadata-name">Group</div>
               <form onSubmit={handleSubmit}>
                 <textarea
                   className="text-box"
                   rows={5}
-                  placeholder="입력..."
+                  placeholder="com.example"
                   onChange={handleTextArea}
                 />
               </form>
-              <div className="dependency">Dependencies</div>
-            </span>
-            <span className="tab-item" title="React" />
-          </Tabs>
+            </div>
+            <div className="project-metadata">
+              <div className="metadata-name">Artifact</div>
+              <form onSubmit={handleSubmit}>
+                <textarea
+                  className="text-box"
+                  rows={5}
+                  placeholder="demo"
+                  onChange={handleTextArea}
+                />
+              </form>
+            </div>
+            <div className="project-metadata">
+              <div className="metadata-name">Name</div>
+              <form onSubmit={handleSubmit}>
+                <textarea
+                  className="text-box"
+                  rows={5}
+                  placeholder="demo"
+                  onChange={handleTextArea}
+                />
+              </form>
+            </div>
+            <div className="project-metadata">
+              <div className="metadata-name">Description</div>
+              <form onSubmit={handleSubmit}>
+                <textarea
+                  className="text-box"
+                  rows={5}
+                  placeholder="Demo project for Spring Boot"
+                  onChange={handleTextArea}
+                />
+              </form>
+            </div>
+            <div className="project-metadata">
+              <div className="metadata-name">Package name</div>
+              <form onSubmit={handleSubmit}>
+                <textarea
+                  className="text-box"
+                  rows={5}
+                  placeholder="com.example.demo"
+                  onChange={handleTextArea}
+                />
+              </form>
+            </div>
+            <div className="dependency">Dependencies</div>
+          </span>
+          <span className="tab-item" title="React" />
+          <button type="button" onClick={modalClose}>
+            Click
+          </button>
+          {modalOpen && <Modal modalClose={modalClose} />}
         </div>
         <div className="confirm-code">
           <div className="confirm-title">CONFIRM CODE</div>
