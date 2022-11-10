c7b4e9fd4754725d017105dbcee66b9c4eca6c19
kkk <rkarud1234@koreatech.ac.kr>
2022-11-10 10:46:44 +0900

    feat: 🎸 Jira 관련 예외처리 추가

--- back/myini-common/src/main/java/com/ssafy/myini/JiraException.java
+++ back/myini-common/src/main/java/com/ssafy/myini/JiraException.java
@@ -2,6 +2,10 @@ package com.ssafy.myini;
 
 public class JiraException  extends RuntimeException{
     public static final String JIRA_FAIL = "지라 이슈등록에 실패했습니다.";
+    public static final String PROJECT_NOT_FOUND = "프로젝트 리스트 조회에 실패했습니다.";
+    public static final String STORY_POINT_NOT_FOUND = "스토리 포인트 정보 조회에 실패했습니다.";
+    public static final String SCREEN_INFO_NOT_FOUND = "스크린 정보 조회에 실패했습니다.";
+    public static final String TAB_INFO_NOT_FOUND = "탭 정보 조회에 실패했습니다.";
     public JiraException(String message) {
         super(message);
     }
