a14c17e368091461cc7c973c5a33373976510b4d
Gungil Bae <97593802+geon-gil@users.noreply.github.com>
2022-11-11 14:29:47 +0900

    feat: 🎸 페이지별 erd 불러오기 대성공

--- front/src/pages/ProjectManage/ERDPage/GenerateVuerd/index.tsx
+++ front/src/pages/ProjectManage/ERDPage/GenerateVuerd/index.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useLayoutEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import AWS from 'aws-sdk';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import './style.scss';
 import { getApi } from 'api';
 
@@ -13,7 +13,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSave } from '@fortawesome/free-regular-svg-icons';
 
 import { assignCurrentErd } from 'modules/vuerd';
-import axios from 'axios';
+import { ERD } from 'modules/erd';
 
 const S3_BUCKET = 'myini/ERD';
 const REGION = 'ap-northeast-2';
@@ -35,42 +35,57 @@ function GenerateVuerd(props: any) {
   const { pid } = useParams();
   const [currentErd, setCurrentErd] = useState<any>({});
   const [jsonToS3, setJsonToS3] = useState<any>({});
+  const dispatch = useDispatch();
   // const { erdName } = props;
+  const dummyTable = {
+    TableModel: {
+      name: 'project_id',
+      comment: '',
+      dataType: 'BIGINT',
+      default: '',
+      option: {
+        autoIncrement: false,
+        primaryKey: true,
+        unique: false,
+        notNull: true,
+      },
+      ui: {
+        active: false,
+        pk: true,
+        fk: false,
+        pfk: false,
+        widthName: 60,
+        widthComment: 60,
+        widthDataType: 60,
+        widthDefault: 60,
+      },
+      id: '271a0c33-39bc-4631-a816-b5cddc297315',
+    },
+  };
 
   useLayoutEffect(() => {
     generateVuerd();
   }, []);
 
   useEffect(() => {
-    // const getProjectJson: any = `https://myini.s3.ap-northeast-2.amazonaws.com/ERD/${pid}.myini.json`;
-    // // 다운로드 방식
-    // const importJsonFromS3 = () => {
-    //   fetch(getProjectJson, { method: 'GET' })
-    //     .then((res) => {
-    //       return res.blob(); // raw 데이터를 받아온다
-    //     })
-    //     .then((blob) => {
-    //       const url = window.URL.createObjectURL(blob); // 받아온 날 상태의 data를 현재 window에서만 사용하는 url로 바꾼다
-    //       const a = document.createElement('a');
-    //       a.href = url;
-    //       document.body.appendChild(a);
-    //       a.click(); // 자동으로 눌러버리기
-    //       setTimeout((_) => {
-    //         window.URL.revokeObjectURL(url); // 해당 url을 더 사용 못하게 날려버린다
-    //       }, 60000);
-    //       a.remove(); // a를 다 사용했으니 지워준다
-    //     })
-    //     .catch((err) => {
-    //       console.error('err: ', err);
-    //     });
-    // };
-    // importJsonFromS3();
+    console.log(erdDiv.current, 'erddiv');
   }, []);
 
   const generateVuerd = () => {
     // vuerd import
     const container: any = document.querySelector('#app-erd');
     let editor: any;
+
+    if (container?.children.item(0)) {
+      container.removeChild(container.children.item(0));
+      editor = document.createElement('erd-editor');
+    } else editor = document.createElement('erd-editor');
+
+    container?.appendChild(editor);
+    const payload: any = {
+      editor: editor,
+    };
+    // payload.editor.context.store.tableState.tables.push(dummyTable);
 
     // const obj: any = {
     //   editor: erdDiv.current.children.item(0),
@@ -94,52 +109,26 @@ function GenerateVuerd(props: any) {
     //   getJson();
     // }
 
-    if (container?.children.item(0)) {
-      container.removeChild(container.children.item(0));
-      editor = document.createElement('erd-editor');
-    } else editor = document.createElement('erd-editor');
+    // disp
+    // dispatch(assignCurrentErd(payload.editor));
 
-    container?.appendChild(editor);
-    const payload = {
-      editor: editor,
+    const getProjectErd = async () => {
+      await getApi(`erds/erdjson/${pid}`)
+        .then((res: any) => {
+          console.log(res.data, 'res');
+          editor.initLoadJson(JSON.stringify(res.data));
+        })
+        .catch((err: any) => {
+          console.log(err, '새로운 프로젝트입니다.');
+        });
     };
-    setCurrentErd(payload);
+    getProjectErd();
 
-    const dummyTable = {
-      name: 'project',
-      comment: '',
-      columns: [
-        {
-          name: 'project_id',
-          comment: '',
-          dataType: 'BIGINT',
-          default: '',
-          option: {
-            autoIncrement: false,
-            primaryKey: true,
-            unique: false,
-            notNull: true,
-          },
-          ui: {
-            active: false,
-            pk: true,
-            fk: false,
-            pfk: false,
-            widthName: 60,
-            widthComment: 60,
-            widthDataType: 60,
-            widthDefault: 60,
-          },
-          id: '271a0c33-39bc-4631-a816-b5cddc297315',
-        },
-      ],
-    };
-    console.log(dummyTable, 'dummy');
-    // payload.editor.context.store.tableState = dummyTable;
-    console.log(payload.editor.context.store.tableState.tables, 'pay');
-    payload.editor.context.store.tableState.tables = dummyTable;
-    console.log(payload.editor.context.store.tableState.tables, 'after');
+    // editor.addEventListener('change', (event: any) => {
+    //   console.log(event.target, '');
+    // });
 
+    // vuerd size
     window.addEventListener('resize', () => {
       editor.width = window.innerWidth * 0.96;
       editor.height = window.innerHeight;
@@ -147,12 +136,11 @@ function GenerateVuerd(props: any) {
     window.dispatchEvent(new Event('resize'));
   };
 
+  // 저장버튼
   const saveBtn = () => {
     const obj: any = {
       editor: erdDiv.current.children.item(0),
     };
-    // const saveCurrentErd = obj.editor.context.store;
-
     // canvas, tabel, memo, relationship
     const isCanvasState = obj.editor.context.store.canvasState;
     const isMemoState = obj.editor.context.store.memoState;
@@ -166,6 +154,7 @@ function GenerateVuerd(props: any) {
     };
     const stateToJson = JSON.stringify(combinedState);
     console.log(isTableState, 'istable');
+    console.log(obj.editor.context, 'editor');
     // fileName
     const fileName = `${pid}.myini.json`;
 
