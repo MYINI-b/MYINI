b4356f0216e3a0fe00aa0cc52737d7d944924a62
hys6078 <hys6078@naver.com>
2022-11-11 10:03:33 +0900

    feat: 🎸 projectmanage 세션 분리를 위한 세팅 준비

--- front/src/pages/ProjectManage/index.tsx
+++ front/src/pages/ProjectManage/index.tsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { useSyncedStore } from '@syncedstore/react';
+import { getYjsValue, syncedStore } from '@syncedstore/core';
+import { WebrtcProvider } from 'y-webrtc';
 
+import { globalStore, ProjectInfo } from 'store/yjsStore';
 import MainHeader from 'components/MainHeader';
 import { setPid } from 'modules/Project';
 import { postApi } from 'api';
@@ -15,12 +19,21 @@ export default function ProjectManage() {
   const [step, setStep] = useState(1);
   const { pid } = useParams();
   const dispatch = useDispatch();
+  const [store, setStore] = useState<any>(useSyncedStore(globalStore));
 
   useEffect(() => {
     const setReduxPid = async () => {
       if (pid === 'new') {
         const { data }: any = await postApi(`/projects`);
         dispatch(data.projectId);
+        const newStore = syncedStore({
+          pjt: {} as ProjectInfo,
+        });
+        new WebrtcProvider(
+          `project${data.projectId}`,
+          getYjsValue(newStore) as any,
+        );
+        setStore(newStore);
       } else {
         dispatch(setPid(pid || ''));
       }
