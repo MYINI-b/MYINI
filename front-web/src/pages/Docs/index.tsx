04a3cdf2e2f2f1b4c27b34f486712475fff34c01
rladnqsl98 <rladnqls98@gmail.com>
2022-11-14 13:30:48 +0900

    feat: 🎸 css 초기세팅
    ✅ Closes: #692

--- front-web/src/pages/Docs/index.tsx
+++ front-web/src/pages/Docs/index.tsx
@@ -40,19 +40,30 @@ export default function Docs() {
             <div className="docs-content">
               <h1>MYINI에 기여해보세요.</h1>
               <hr />
-              <br />
-              <h3>시작하기 전</h3>
-              <br />
-              <span>
-                저희 MYINI는 올바른 오픈소스 문화를 토대로 컨트리부트를 진행하고
-                있습니다.
-              </span>
-              <span>
-                <a href="https://www.contributor-covenant.org/ko/version/1/4/code-of-conduct/">
-                  컨트리부트 행동강령
-                </a>
-                을 읽고 진행해주세요!
-              </span>
+              <div className="docs-content-item">
+                <h3>시작하기 전</h3>
+                <span>
+                  저희 MYINI는 올바른 오픈소스 문화를 토대로 컨트리부트를
+                  진행하고 있습니다.
+                </span>
+                <span>
+                  <a href="https://www.contributor-covenant.org/ko/version/1/4/code-of-conduct/">
+                    컨트리부트 행동강령
+                  </a>
+                  을 읽고 진행해주세요!
+                </span>
+              </div>
+              <hr />
+              <div className="docs-content-item">
+                <span>
+                  저희 MYINI는 아래 과정을 통해 컨트리뷰션을 진행하고 있습니다.
+                </span>
+                <br />
+                <span>
+                  코드 수정이 아니더라도 다양한 의견 / 아이디어 등을 통해
+                  MYINI에 기여해주세요.
+                </span>
+              </div>
               {/* <span>여기로 연락주세용~~~~</span> */}
             </div>
           ) : (
