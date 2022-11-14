09807222c888ccaa2659056b2d785db7e75af300
rladnqsl98 <rladnqls98@gmail.com>
2022-11-14 14:00:11 +0900

    style: 💄 docs페이지 중간저장

--- front-web/src/pages/Docs/index.tsx
+++ front-web/src/pages/Docs/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Header from 'component/Header';
+import fork from 'assets/fork.png';
 import './style.scss';
 
 export default function Docs() {
@@ -39,30 +40,48 @@ export default function Docs() {
           ) : docsStep === 1 ? (
             <div className="docs-content">
               <h1>MYINI에 기여해보세요.</h1>
+              <span>
+                더 자세한 내용은
+                <a href="https://lab.ssafy.com/s07-final/S07P31B203">
+                  &nbsp;이곳
+                </a>
+                에서 확인해주세요.
+              </span>
               <hr />
               <div className="docs-content-item">
                 <h3>시작하기 전</h3>
                 <span>
-                  저희 MYINI는 올바른 오픈소스 문화를 토대로 컨트리부트를
-                  진행하고 있습니다.
-                </span>
-                <span>
+                  ● MYINI는 올바른 오픈소스 문화를 토대로 컨트리부트를 진행하고
+                  있습니다.&nbsp;&nbsp;
                   <a href="https://www.contributor-covenant.org/ko/version/1/4/code-of-conduct/">
                     컨트리부트 행동강령
                   </a>
                   을 읽고 진행해주세요!
                 </span>
+                <span>
+                  ● MYINI는 아래 과정을 통해 컨트리뷰션을 진행하고 있습니다.
+                </span>
+                <span>
+                  ● 코드 수정이 아니더라도 다양한 의견 / 아이디어 등을 통해
+                  MYINI에 기여해주세요.
+                </span>
               </div>
               <hr />
               <div className="docs-content-item">
+                <h4>0. MYINI와 동일하게 개발 버전을 맞춰주세요.</h4>
                 <span>
-                  저희 MYINI는 아래 과정을 통해 컨트리뷰션을 진행하고 있습니다.
+                  ● <b>Backend</b>&nbsp;&nbsp;Java : 11,&nbsp;&nbsp;SpringBoot :
+                  2.7.5,&nbsp;&nbsp;Gradle : 7.5.1
                 </span>
-                <br />
                 <span>
-                  코드 수정이 아니더라도 다양한 의견 / 아이디어 등을 통해
-                  MYINI에 기여해주세요.
+                  ● <b>Frontend</b>&nbsp;&nbsp;Java : 11,&nbsp;&nbsp;SpringBoot
+                  : 2.7.5,&nbsp;&nbsp;Gradle : 7.5.1
                 </span>
+              </div>
+              <hr />
+              <div className="docs-content-item">
+                <h4>1. 우측 상단의 Fork를 눌러 Git Repository를 복제하세요.</h4>
+                <img src={fork} alt="fork" />
               </div>
               {/* <span>여기로 연락주세용~~~~</span> */}
             </div>
