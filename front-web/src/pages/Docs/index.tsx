4063507b728ba7b11a6357902f3d6d9b3d311376
rladnqsl98 <rladnqls98@gmail.com>
2022-11-14 14:34:59 +0900

    feat: 🎸 member jira email return

--- front-web/src/pages/Docs/index.tsx
+++ front-web/src/pages/Docs/index.tsx
@@ -50,33 +50,49 @@ export default function Docs() {
               <hr />
               <div className="docs-content-item">
                 <h3>시작하기 전</h3>
-                <span>
-                  ● MYINI는 올바른 오픈소스 문화를 토대로 컨트리부트를 진행하고
-                  있습니다.&nbsp;&nbsp;
-                  <a href="https://www.contributor-covenant.org/ko/version/1/4/code-of-conduct/">
-                    컨트리부트 행동강령
-                  </a>
-                  을 읽고 진행해주세요!
-                </span>
-                <span>
-                  ● MYINI는 아래 과정을 통해 컨트리뷰션을 진행하고 있습니다.
-                </span>
-                <span>
-                  ● 코드 수정이 아니더라도 다양한 의견 / 아이디어 등을 통해
-                  MYINI에 기여해주세요.
-                </span>
+                <ul>
+                  <li>
+                    <span>
+                      ● MYINI는 올바른 오픈소스 문화를 토대로 컨트리부트를
+                      진행하고 있습니다.&nbsp;&nbsp;
+                      <a href="https://www.contributor-covenant.org/ko/version/1/4/code-of-conduct/">
+                        컨트리부트 행동강령
+                      </a>
+                      을 읽고 진행해주세요!
+                    </span>
+                  </li>
+                  <li>
+                    <span>
+                      ● MYINI는 아래 과정을 통해 컨트리뷰션을 진행하고 있습니다.
+                    </span>
+                  </li>
+                  <li>
+                    <span>
+                      ● 코드 수정이 아니더라도 다양한 의견 / 아이디어 등을 통해
+                      MYINI에 기여해주세요.
+                    </span>
+                  </li>
+                </ul>
               </div>
               <hr />
               <div className="docs-content-item">
                 <h4>0. MYINI와 동일하게 개발 버전을 맞춰주세요.</h4>
-                <span>
-                  ● <b>Backend</b>&nbsp;&nbsp;Java : 11,&nbsp;&nbsp;SpringBoot :
-                  2.7.5,&nbsp;&nbsp;Gradle : 7.5.1
-                </span>
-                <span>
-                  ● <b>Frontend</b>&nbsp;&nbsp;Java : 11,&nbsp;&nbsp;SpringBoot
-                  : 2.7.5,&nbsp;&nbsp;Gradle : 7.5.1
-                </span>
+                <ul>
+                  <li>
+                    <span>
+                      ● <b>Backend</b>&nbsp;&nbsp;Java :
+                      11,&nbsp;&nbsp;SpringBoot : 2.7.5,&nbsp;&nbsp;Gradle :
+                      7.5.1
+                    </span>
+                  </li>
+                  <li>
+                    <span>
+                      ● <b>Frontend</b>&nbsp;&nbsp;Java :
+                      11,&nbsp;&nbsp;SpringBoot : 2.7.5,&nbsp;&nbsp;Gradle :
+                      7.5.1
+                    </span>
+                  </li>
+                </ul>
               </div>
               <hr />
               <div className="docs-content-item">
