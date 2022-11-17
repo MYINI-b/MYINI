b86ce6289ffeb85a90d665e94f9e110086638788
이성재 <lsuksa1@naver.com>
2022-11-17 09:40:52 +0900

    ✨ feat: electron dev최신화하면서 사용

--- front/src/pages/App.tsx
+++ front/src/pages/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
@@ -12,14 +12,14 @@ import Social from './User/Social';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/social/redirect" element={<Social />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/project/:pid" element={<ProjectManage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
