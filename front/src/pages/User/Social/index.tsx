50dd4d9d89aaad5763a4dad15a08d9b68a0d95ba
hys6078 <hys6078@naver.com>
2022-11-21 00:27:14 +0900

    fix: 🐛 로그인 리다이렉션 수정4

--- front/src/pages/User/Social/index.tsx
+++ front/src/pages/User/Social/index.tsx
@@ -1,18 +1,19 @@
 /* eslint-disable no-console */
 /* eslint-disable no-alert */
 import React, { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export default function Social() {
   const token = useLocation().search.split('=')[1];
-  const navigate = useNavigate();
 
   useEffect(() => {
     localStorage.setItem('accessToken', token);
     const isApp = localStorage.getItem('isApp');
 
     if (isApp === 'true') window.location.href = '/main';
-    else window.location.href = 'https://k7b203.p.ssafy.io/';
+    else {
+      window.location.href = 'https://k7b203.p.ssafy.io/';
+    }
   }, []);
 
   return <div />;
